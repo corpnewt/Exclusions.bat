73164853d04a7ed74e5d310d349f5d8466707c2a
CorpNewt <12772521+corpnewt@users.noreply.github.com>
2019-10-17 05:03:21 -0500

    Slim calls, pass args, and preserve cd

--- Exclusions.bat
+++ Exclusions.bat
@@ -8,18 +8,17 @@ NET FILE 1>NUL 2>NUL
 if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges )
 
 :getPrivileges
-if '%~1'=='ELEV' (shift & goto main)
-ECHO.
-
 setlocal DisableDelayedExpansion
 set "batchPath=%~0"
 setlocal EnableDelayedExpansion
 ECHO Set UAC = CreateObject^("Shell.Application"^) > "%temp%\OEgetPrivileges.vbs"
-ECHO UAC.ShellExecute "!batchPath!", "ELEV", "", "runas", 1 >> "%temp%\OEgetPrivileges.vbs"
+ECHO UAC.ShellExecute "!batchPath!", "%*", "", "runas", 1 >> "%temp%\OEgetPrivileges.vbs"
 "%temp%\OEgetPrivileges.vbs"
 exit /B
 
 :gotPrivileges
+:: set the current directory to the batch file location
+cd /d %~dp0
 ::::::::::::::::::::::::::::
 ::START
 ::::::::::::::::::::::::::::
